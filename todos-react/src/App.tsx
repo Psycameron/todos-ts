d82d08fc3105f7e91a475db7dd740d0387204b6e
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-10-19 22:00:17 +0300

    feat(): Added markup and logic for creating tasks

--- todos-react/src/App.tsx
+++ todos-react/src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import TaskCreator from "./components/TaskCreator/TaskCreator";
+import TodoList from "./components/TodoList/TodoList";
 import "./App.css";
 
+interface ITodos {
+  id: number;
+  title: string;
+}
+
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<ITodos[]>([]);
+
+  function onSubmit(title) {
+    const newTodo = { id: Date.now(), title };
+
+    setTodos([...todos, newTodo]);
+  }
+
+  return (
+    <div className="App">
+      <TaskCreator onSubmit={onSubmit} />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
--- todos-react/src/components/TaskCreator/TaskCreator.tsx
+++ todos-react/src/components/TaskCreator/TaskCreator.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function TodoButton() {
-  return <div>TodoButton</div>;
+export default function TaskCreator({ onSubmit }) {
+  const [title, setTitle] = useState("");
+
+  function onChange(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    onSubmit(title);
+    setTitle("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        required
+        onChange={onChange}
+        value={title}
+      />
+      <button type="submit">Create task</button>
+    </form>
+  );
 }
