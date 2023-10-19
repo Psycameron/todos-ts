f88cbb3009c04737b3ea9585f616d9886da704fc
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-10-19 21:16:42 +0300

    chore(): fix some files

--- todos-react/src/components/DeleteButton/DeleteButton.tsx
+++ todos-react/src/components/DeleteButton/DeleteButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function TaskCreator() {
-  return <div>TaskCreator</div>;
+export default function DeleteButton() {
+  return <div>DeleteButton</div>;
 }
